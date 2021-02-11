f4ecb3b8ca5b0faa1fe7779d736c29542c636220
MichaelAbe <michael.k.abe@gmail.com>
2021-02-11 15:30:19 -0500

    commit tree 23e17490e9c0a24212fa332559eef2fd8062373b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    let card = []
+    for (let i = 0; i < names.length; i++){
+        card.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return card
+}
+
+function countDown( n ) {
+    while ( n > 0 ) {
+      console.log( n );
+      n -= 1;
+    }
+    console.log( n );
+  }
